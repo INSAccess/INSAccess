d3f19702856c04a5665f21dfc04a3be803e5142c
ImJustCookie <raphael@senellart.com>
2025-03-13 18:26:24 +0100

    finished cleaned code

--- backend/backend/utils/fetch.py
+++ backend/backend/utils/fetch.py
@@ -33,41 +33,47 @@ Notes:
 
 """
 
+
 from xml.etree import ElementTree
-import sys
-import re
-import html
-import itertools
-import requests
-import calendar
+import sys, os, re, html, itertools, \
+    requests, calendar, logging, json
+
 from datetime import datetime, timedelta
-import logging
-
-def get_item_to_be_removed() -> list:
-    """ return the constant list that should be removed
-        from the list of groups
-        (they are weird tags that are unwanted in our case)"""
-    return ['examens']
-
-def get_depart_list() -> list:
-    """ return the department list"""
-    return ["CGC", "EP", "GCU", "GM", "GPGR", "ITI",\
-            "MECA", "PERF-E", "PERF-II", "PERF-ISP", "PERF-NI"]
 
 
+CONFIG_PATH = os.path.join(os.path.dirname((os.path.dirname(os.path.dirname(__file__)))),"config/insa_config.json")
+
+def load_config():
+    """Loads the configuration file from the project config folder."""
+    with open(CONFIG_PATH, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+CONFIG = load_config()  
 
 def xml_to_list(url : str) -> list:
-    """take a url (of the agenda of insa (agenda.insa-rouen.fr)
-    and parse it to useful components
+    """
+    Parses an INSA Rouen agenda URL and extracts useful schedule components.
 
+    This function takes a URL from the INSA Rouen agenda (agenda.insa-rouen.fr),
+    fetches the schedule data, and extracts relevant details into a structured format.
 
-    Keyword arguments:
-    url -- the url to fetch
-    url -- depart
-    Return: return_description
+    Parameters:
+        url (str): The URL of the agenda to fetch and parse.
+
+    Returns:
+       list of tuples: A list where each tuple represents an event with the following fields:
+            - uid (str): Unique identifier of the event.
+            - date (str): Date of the event in 'YYYY-MM-DD' format.
+            - start_hour (str): Start time of the event (e.g., 'HH:MM').
+            - end_hour (str): End time of the event (e.g., 'HH:MM').
+            - title (str): Title or subject of the event.
+            - locations (list of str): List of locations where the event takes place.
+            - teacher_list (list of str): List of teachers associated with the event.
+            - group_td (list of str): List of TD (tutorial) groups attending.
+            - group_depart (list of str): List of department groups attending.
     """
 
-    response = requests.get(url, timeout=5)
+    response = requests.get(url, timeout = 5)
 
     if response.status_code == 200: #request is successful
         response.encoding = 'utf-8'
@@ -76,20 +82,21 @@ def xml_to_list(url : str) -> list:
         try:
             root = ElementTree.fromstring(xml_data)
         except ElementTree.ParseError as err:
-            print(f"XML parsing error: {err}")
+            logging.error(f"XML parsing error: {err}")
             return []
 
-        namespaces = {
-            "ev": "http://purl.org/rss/1.0/modules/event/"
-        }
+
+        # namespaces = {k: v for k, v in root.attrib.items() if k.startswith("xmlns")}
+        namespaces = {"ev" : "http://purl.org/rss/1.0/modules/event/"}
+        
         output = []
 
         for item in root.findall("./channel/item"):
-            uid = item_to_string(item.find("guid"))
-            title = item_to_string(item.find("title"))
+            uid = item_to_string(item.find("guid", namespaces))
+            title = item_to_string(item.find("title", namespaces))
             date, start_hour = item_to_string(item.find("ev:startdate", namespaces)).split('T')
             _, end_hour = item_to_string(item.find("ev:enddate", namespaces)).split('T')
-            description = item_to_string(item.find("description"))
+            description = item_to_string(item.find("description", namespaces))
             locations = item_to_string(item.find("ev:location", namespaces)).split('%2C')
 
             uid = extract_uid(uid)
@@ -103,40 +110,46 @@ def xml_to_list(url : str) -> list:
 
         return output
 
-    print(f"Failed to fetch XML data. HTTP Status Code: {response.status_code}")
+    logging.error(f"Failed to fetch XML data. HTTP Status Code: {response.status_code}")
+    
     return []
 
 
 def get_calendar_data(current_year :str, department : str ,\
                        depart_year : str, date : str, period :str) -> list :
     """
-    get_calendar_data fetch the processed data from insa ALL PARAMETERS ARE STR.
+    Fetches and processes calendar data from INSA for a given school year, department, and period.
 
-    :param current_year: the current school year
-    (i.e if 2024-2025 then = 2024)
+    This function retrieves schedule information for a specified department, year, 
+    and time period from the INSA calendar system.
 
-    :param department: the department
-    ("CGC", "EP", "GCU", "GM", "GPGR", "ITI", "MECA",
-    "PERF-E", "PERF-II", "PERF-ISP", "PERF-NI" ...)
+    Parameters:
+        current_year (str): The current academic year (e.g., "2024" for the 2024-2025 school year).
+        department (str): The department code (e.g., "CGC", "EP", "GCU", "GM", "GPGR", 
+                        "ITI", "MECA", "PERF-E", "PERF-II", "PERF-ISP", "PERF-NI", etc.).
+        depart_year (str): The year in the department (e.g., "ITI3" for the third year in ITI).
+        date (str): The specific date to fetch data for. If fetching data for a week or month, 
+                    provide a date within that period.
+        period (str): The period to fetch data for (e.g., "day", "week", "month").
 
-    :param depart_year: the year in the department
-    (i.e ITI3 if third year and in ITI)
-
-    :param date: the date you want to fetch
-    (if you fetch for a week or month, give the month/week where the date is)
-
-    :param period: the period you want
-    (i.e day, week, month )
-
-    :return: return a list of tuples looking like so (uid, date, start_hour, end_hour, title, locations,\
-                            teacher_list, group_td, group_depart)
+    Returns:
+        list of tuples: A list where each tuple represents an event with the following structure:
+            - uid (str): Unique identifier of the event.
+            - date (str): Date of the event in 'YYYY-MM-DD' format.
+            - start_hour (str): Start time of the event (e.g., 'HH:MM').
+            - end_hour (str): End time of the event (e.g., 'HH:MM').
+            - title (str): Title or subject of the event.
+            - locations (list of str): List of locations where the event takes place.
+            - teacher_list (list of str): List of teachers associated with the event.
+            - group_td (list of str): List of TD (tutorial) groups attending.
+            - group_depart (list of str): List of department groups attending.
     """
-
-    depart_list = get_depart_list()
-    depart_years = ["3", "4", "5"]
-    prepa_name = "SPTI"
-    prepa_years = ["1", "2"]
-    list_of_period = ["day", "week", "month"]
+    
+    depart_list = CONFIG["department_list"]
+    depart_years = CONFIG["years_for_department"]
+    prepa_name = CONFIG["prepa_name"]
+    prepa_years = CONFIG["years_for_prepa"]
+    list_of_period = CONFIG["periods_list"]
     
 
     if period in list_of_period:
@@ -147,28 +160,21 @@ def get_calendar_data(current_year :str, department : str ,\
                         department, depart_year, "&cpath=&rssview=", period, "&getdate=", date])
 
             return xml_to_list(url)
+        
         logging.error(f"Wrong department or depart_year given, got {department} and {depart_year}, expected\
             {depart_list} or {prepa_name} and {depart_years} or {prepa_years}")
         
     logging.error(f"Wrong period was given, expected one of those :{list_of_period} but got {period}")
 
-    print(f"ERROR : wrong arguments given : department = {depart_list},\
-            3 <= year <= 5, period = {list_of_period}" )
     return []
 
 
 def extract_uid(uid):
-    pattern = r'.*uid=(.*)'
-    try : 
-        parsed_uid = re.findall(pattern, uid)[0]
-    except IndexError as err :
-        print(f"List Index error when regex:{err}")
-        return ''
-    return parsed_uid
+    """ extract the uid from url"""
+    return re.findall(r'.*uid=(.*)', uid)[0]
 
 def clean_title(title):
     """ parse the title of the fetched xml"""
-
     class_name = title.split(': ')[1]
     class_name = class_name.replace('-', ' ')
     return class_name
@@ -180,11 +186,10 @@ def parse_description(description):
     because of the specific XML structure of Insa"""
 
     #pre-treatement of the description
-    pattern_for_parsing = r'(?<=<br/>).*'
     try :
-        desc_string = re.findall(pattern_for_parsing, description )[0]
+        desc_string = re.findall(r'(?<=<br/>).*', description )[0]
     except IndexError as err:
-        print(f"List Index error when regex: {err}")
+        logging.error(f"List Index error when regex: {err}")
         return [],"" ,[]
 
     desc_item_list = desc_string.split(r'<br/>')[1:-2]
@@ -196,11 +201,9 @@ def parse_description(description):
     name_list, desc_item_list = pop_multiple_element(desc_item_list, list_of_indexes)
 
 
-    # separate the department and tdgroup from the description into 2 list
-    depart_years =['3', '4', '5']
-    department_set = set(map(''.join, itertools.product(get_depart_list(),depart_years)))
-    department_set.add('STPI1')
-    department_set.add('STPI2')
+    # Divide the department and tdgroup from the description into 2 list
+    department_set = set(list(map(''.join, itertools.product(CONFIG["department_list"], CONFIG["years_for_department"]))) + 
+                         list(map(''.join, itertools.product(CONFIG["prepa_name"], CONFIG["years_for_prepa"]))))
 
     depart_in_desc, td_group_in_desc = [],[]
     for item in desc_item_list:
@@ -210,10 +213,10 @@ def parse_description(description):
             td_group_in_desc.append(item)
 
 
-    #remove weird specific string that can appear in the td group
+    #Remove weird specific string that can appear in the td group
     list_of_indexes_filter=[]
     for index, item in enumerate(td_group_in_desc):
-        if item in get_item_to_be_removed():
+        if item in CONFIG["misc_item_in_description"]:
             list_of_indexes_filter.append(index)
 
     _, td_group_in_desc = pop_multiple_element(td_group_in_desc,list_of_indexes_filter)
@@ -234,26 +237,24 @@ def item_to_string(item):
     """transform the item to a string if possible"""
     return item.text if item is not None else ""
 
-
-
 def get_last_week_start(year, month):
     """Return the date string (YYYYMMDD) for the start of the last week of the given month."""
     last_day = calendar.monthrange(year, int(month))[1]  # Get last day of the month
     last_day_date = datetime(year, int(month), last_day)
-    start_of_last_week = last_day_date - timedelta(days=6)  # Start of the last week
+    start_of_last_week = last_day_date - timedelta(days = 6)  # Start of the last week
     return start_of_last_week.strftime("%Y%m%d")
 
-def get_yearly_calendar_data(year, department, depart_year, months,remove_one_to_year):
+def get_yearly_calendar_data(year, department, depart_year, months, remove_one_to_year):
     data_list = []
     start_year = year -1 if remove_one_to_year else year
     for month in months:
         # Fetch the entire month
         data_list += get_calendar_data(
-            str(start_year), department, depart_year, f"{year}{month}01", "month"
+            str(start_year), department, depart_year, f"{year}{month}01", CONFIG["periods_list"][2]
         )
         # Fetch the last week of the month because the last few days are sometimes not in the xml given
         data_list += get_calendar_data(
-            str(start_year), department, depart_year, get_last_week_start(year, month), "week"
+            str(start_year), department, depart_year, get_last_week_start(year, month), CONFIG["periods_list"][1]
         )
     return data_list
 
@@ -317,6 +318,8 @@ def replace_entities(content, replacements):
         content = content.replace(entity, replacement)
 
     return content
+
+
 
 #---------------LIST PROCESSING UTILS-------------------------#
 
