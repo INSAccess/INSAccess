bf3e080b77139724427ec479bd879ada838fb473
ImJustCookie <raphael@senellart.com>
2025-04-14 14:27:57 +0200

    finished custom ics route

--- backend/apis/views/ics_views.py
+++ backend/apis/views/ics_views.py
@@ -1,5 +1,4 @@
-import datetime
-from ics import Calendar, Event
+from icalendar import Calendar, Event
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
@@ -15,34 +14,43 @@ def generate_ics(request, user_id):
     Return: a ics file with all the event of the user
     """
 
-    user = User.objects.get(id=user_id)
+    # user = User.objects.get(id=user_id)
     user = request.user
-    start_of_year =  datetime.datetime.today().replace(month=1,day=1)  # First day of the year
-    end_of_year = (start_of_year + datetime.timedelta(days=400)).replace(day=1,month=1)\
-                                - datetime.timedelta(days=1)  # Last day of the year
-
     user_tds = user.userprofile.link_td.all()
-    classes = InsaClass.objects.filter(link_td__in=user_tds,
-                                       date__range =[start_of_year, end_of_year]).distinct()
+    classes = InsaClass.objects.filter(link_td__in=user_tds).distinct()
 
     cal = Calendar()
 
+    cal.add("method", "REQUEST")
+    cal.add("prodid", "-//INSAccess/version 1.0")
+    cal.add("x-wr-calname;value=text", user_id)
+    cal.add("calscale", "GREGORIAN")
+    cal.add("version", "1.0")
     for event in classes:
         e = Event()
-        e.name = event.desc
-        e.begin = datetime.datetime.combine(event.date, event.start_hour).strftime("%Y%m%dT%H%M%SZ")
-        e.end = datetime.datetime.combine(event.date, event.end_hour).strftime("%Y%m%dT%H%M%SZ")
+        e.add("dtstamp", event.time_stamp)
+        e.add("dtstart", event.start_hour)
+        e.add("dtend", event.end_hour)
+        e.add("summary", event.desc)
+        
         rooms = [room.name for room in event.link_room.all()]
-        e.location = ",".join(rooms)
-
+        e.add("location", ",".join(rooms))
+        
+        td_tags = [td.name for td in event.link_td.all()]
         departments = [depart.name for depart in event.link_depart.all()]
         teachers = [teacher.name for teacher in event.link_teacher.all()]
-        final_description = [event.desc] + teachers + departments
+        final_description = td_tags + teachers + departments
         nl = "\n"
-        e.description = f"{nl}{nl}{nl.join(final_description)}{nl}"
-        cal.events.add(e)
+        e.add("description", f"{nl}{nl}{nl.join(final_description)}{nl}")
+        
+        e.add('uid', event.uid)
+        e.add("created", event.time_created)
+        e.add("last-modified", event.time_last_modified)
+        e.add("sequence", event.sequence)
 
-    response = HttpResponse(str(cal), content_type="text/calendar")
-    response['Content-Disposition'] = 'inline; filename=calendar.ics'
+        cal.add_component(e)
+
+    response = HttpResponse(cal.to_ical().decode('utf-8'), content_type="text/calendar")
+    response['Content-Disposition'] = f'inline; filename=calendar_{user_id}.ics'
 
     return response
