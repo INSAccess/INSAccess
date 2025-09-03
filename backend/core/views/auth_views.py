6036e87c2336dc5f90ac2767f2ce7bdd7bc0e68a
ImJustCookie <raphael@senellart.com>
2025-09-03 18:03:30 +0200

    removed test routes

--- backend/core/views/auth_views.py
+++ backend/core/views/auth_views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from core.models import UserProfile, EnumColorTheme
-from core.utils.fetch_ics import fetch_department
-from core.utils.db_insertor import insert_list_record
+from core.models import UserProfile
 from django.conf import settings
 
-from django_cas_ng.views import LoginView
 import logging
 
 logger = logging.getLogger(__name__)
@@ -20,7 +17,7 @@ def get_frontend_url():
     if not settings.DEBUG:
         return f"http://{settings.HOST_IP}"
     else:
-        return f"http://{settings.HOST_IP}:8000/"
+        return f"http://{settings.HOST_IP}:3000/"
 
 
 @login_required
@@ -33,12 +30,3 @@ def finalize(request):
 
     return redirect(get_frontend_url())
 
-def test_insertion(request):
-    records = fetch_department("ITI" ,"4")
-    insert_list_record(records)
-    return redirect(get_frontend_url())
-
-def seed_database(request):
-    from core.fixtures.seed_enums import run_seeder
-    run_seeder()
-    return redirect(get_frontend_url())
