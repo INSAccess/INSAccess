b0c5c86f9744192034dde024e2adb5aa9975d20e
ImJustCookie <raphael@senellart.com>
2025-09-03 13:34:36 +0200

    started to work on single server for prod

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,6 +144,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles" / "static",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,33 +1,22 @@
 
 from django.contrib import admin
-from django.urls import include, path
-
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-from rest_framework import permissions
+from django.urls import include, path, re_path
 from core.admin import custom_admin_site
+from django.views.generic import TemplateView
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="My API",
-        default_version="v1",
-        description="API documentation for my Django app",
-        contact=openapi.Contact(email="support@example.com"),
-        license=openapi.License(name="CC BY-NC-SA 4.0"),
-    ),
-    public=True,
-    permission_classes=(permissions.IsAdminUser,),
-)
 
 urlpatterns = [
-    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-ui"),
-    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="redoc"),
-    path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    path("swagger.yaml", schema_view.without_ui(cache_timeout=0), name="schema-yaml"),
-    
     path('admin/default', admin.site.urls),
     path('admin/custom', custom_admin_site.urls),
     path('api/', include('core.urls.api_urls')),
     path('authentification/', include('core.urls.auth_urls')),
-    path('ics/', include('core.urls.ics_urls'))
+    path('ics/', include('core.urls.ics_urls')),
+    path('', TemplateView.as_view(template_name='index.html'))
+]
+
+urlpatterns += [
+    re_path(
+        r'^(?!api/|admin/|authentification/|ics/|static/|media/).*$',
+        TemplateView.as_view(template_name='index.html')
+    ),
 ]
