f17b5261f5485ef4a56a0cd8d67e92323272bf44
ImJustCookie <raphael@senellart.com>
2025-11-05 12:53:28 +0100

    fixed typo again

--- backend/core/views/auth_views.py
+++ backend/core/views/auth_views.py
@@ -33,9 +33,9 @@ def finalize(request):
         normalized_cas_tds = [td.strip().lower() for td in subscribed_tds]
 
         tds_in_db = [
-            td.strip().lower()
+            td
             for td in GroupTD.objects.all()
-            if td.name in normalized_cas_tds
+            if td.name.strip().lower() in normalized_cas_tds
         ]
 
         user_profile.link_td.add(*tds_in_db)
